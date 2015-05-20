f587e7c654193816c540447bae870f7a5a14e14b
trijeetm <trijeetm@gmail.com>
2015-05-20 01:25:46 -0700

    Copied SonicCanvas repository as starting point

--- Native Stash/GLoiler/DismissSegue.h
+++
@@ -1,5 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface DismissSegue : UIStoryboardSegue
-
-@end
--- Native Stash/Pods/Target Support Files/Pods-GLoiler-Firebase/Pods-GLoiler-Firebase-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GLoiler_Firebase : NSObject
-@end
-@implementation PodsDummy_Pods_GLoiler_Firebase
-@end
